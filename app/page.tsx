763c280fcf220235713d85cc5e4b5217e426eca7
zachmiracleflights <zmcmullen@miracleflights.org>
2023-05-23 14:42:37 -0700

    [Fix] Board styling

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Board from "./components/Board"
 
 export default function Home() {
   return (
-    <main className="h-screen w-screen text-white flex flex-col justify-center items-center space-y-5 p-3 text-center">
+    <main className="h-screen w-screen text-white flex flex-col space-y-5 p-10 text-center items-center max-w-full">
       <div className="shadow-2xl lg:m-20 rounded-md bg-cover bg-[url('/HalfHeart.png')] bg-right-bottom">
           {/* @ts-expect-error Async Server Component */}
           <Board route={"departures"}/>
