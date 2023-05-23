5519d310d8bcf147c2a489f517548bfbe46d15d3
zachmiracleflights <zmcmullen@miracleflights.org>
2023-05-23 12:38:00 -0700

    [Update] Board Image

--- app/page.tsx
+++ app/page.tsx
@@ -4,7 +4,7 @@ import Image from "next/image"
 export default function Home() {
   return (
     <main className="h-screen w-screen text-white flex flex-col justify-center items-center space-y-5 p-3 text-center">
-      <div className="shadow-2xl lg:m-20 rounded-md bg-cover bg-no-repeat">
+      <div className="shadow-2xl lg:m-20 rounded-md bg-cover bg-[url('/HalfHeart.png')] bg-right-bottom">
           {/* <Image  
             src="/HalfHeart.png"
             alt="half-heart"
